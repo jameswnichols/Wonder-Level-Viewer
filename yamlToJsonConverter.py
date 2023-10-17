1cad47976998cd73a4a8f35ca7ffcff6b8549270
James Nichols <jnichols.tigley@gmail.com>
2023-10-17 16:03:31 +0100

    Converts toolbox yaml to python json.

---
+++ yamlToJsonConverter.py
@@ -0,0 +1,38 @@
+import yaml
+import json
+import regex
+import os
+from io import BytesIO
+
+pattern = regex.compile("!..? ")
+
+filename = "Course2.yaml"#input("Enter filepath: ")
+
+finalLines = []
+
+with open(filename,"r") as f:
+    fileLines = f.readlines()
+
+for line in fileLines:
+    collisions = pattern.finditer(line)
+    newLine = line
+    for collision in collisions:
+        start, end = collision.span()
+        tag = line[start:end]
+        newLine = newLine.replace(tag, "")
+    
+    finalLines.append(newLine)
+
+with open("temp.yaml","w") as f:
+    f.writelines(finalLines)
+
+with open("temp.yaml","r") as f:
+    yamlData = yaml.safe_load(f)
+
+newFilename = filename[:filename.rfind(".")]+".json"
+
+with open(newFilename,"w") as f:
+    json.dump(yamlData, f)
+    
+
+
