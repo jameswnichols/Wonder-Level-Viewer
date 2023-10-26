4c8187f7b315dc9e8d0b55f0f8c3df69a319b372
James Nichols <jnichols.tigley@gmail.com>
2023-10-26 12:21:31 +0100

    Made it so can navigate dict with list.

--- toolkitYamlToJson.py
+++ toolkitYamlToJson.py
@@ -2,26 +2,30 @@ import json
 
 #5 places of float precision.
 
+def navigatePath(data, path):
+    currentLevel = data
+    for level in path:
+        currentLevel = currentLevel[level]
+    return currentLevel
+
 yamlData = None
 
-levelData = {}
-
-lastIndent = 0
+levelData = {"a":{"b":{"c":[]}}}
 
 currentPath = []
+
+lastIndent = 0
 
 with open("TESTING.yaml","r") as f:
     yamlData = f.readlines()
 
-for line in yamlData:
+# for line in yamlData:
 
-    currentIndentLevel = 0
+#     currentIndentLevel = 0
 
-    for i, char in enumerate(line):
-        if char == " ":
-            currentIndentLevel += 1
+#     for i, char in enumerate(line):
+#         if char == " ":
+#             currentIndentLevel += 1
     
-    currentIndentLevel //= 2
-
-    if currentIndentLevel >
+#     currentIndentLevel //= 2
         
