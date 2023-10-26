c25ac269cf56dec66f6c90fa81205aff5b8ba9a3
James Nichols <jnichols.tigley@gmail.com>
2023-10-26 18:35:29 +0100

    Deleted it all and started fresh lol.

--- toolkitYamlToJson.py
+++ toolkitYamlToJson.py
@@ -7,16 +7,13 @@ class LevelData:
         self.levelData = {}
         self.currentPath = {}
 
-    def navigatePath(self, stepBackAmount : int = 0, ignoreIndex : bool = False):
+    def navigatePath(self):
         counter = 0
         currentLevel = self.levelData
         for level, val in self.currentPath.items():
             isList, index = val["isList"], val["index"]
 
-            if len(self.currentPath) - 1 - (stepBackAmount - 1) == counter:
-                return currentLevel
-
-            if (not isList) or ignoreIndex:
+            if (not isList):
                 currentLevel = currentLevel[level]
             else:
                 currentLevel = currentLevel[level][index]
@@ -24,36 +21,6 @@ class LevelData:
             counter += 1
 
         return currentLevel
-    
-    def convertTopToList(self, indentLevel):
-        prevData = self.navigatePath(1)
-        prevData[self.getTopOfPath()] = [{}]
-        self.currentPath[self.getTopOfPath()]["isList"] = True
-        self.currentPath[self.getTopOfPath()]["itemIndent"] = indentLevel
-
-    def isTopList(self):
-        return self.currentPath[self.getTopOfPath()]["isList"]
-    
-    def incrementIndexOnPath(self, key):
-        self.currentPath[key]["index"] += 1
-        self.navigatePath(0,True).append({})
-
-    def getTopOfPath(self):
-        return list(self.currentPath.keys())[-1]
-    
-    def addToPath(self, key):
-        self.currentPath[key] = {"isList":False, "index": -1, "itemIndent":-1}
-
-    def removeFromPath(self, amount):
-        for i in range(0, amount):
-            del self.currentPath[self.getTopOfPath()]
-
-    def getTopIndentLevel(self):
-        return self.currentPath[self.getTopOfPath()]["itemIndent"]
-
-
-def extractKeyValuePair(text):
-    pass
 
 yamlData = None
 
@@ -79,69 +46,20 @@ for i in range(0, 41):
 
     firstCharacter = line[leadingSpaces]
 
-    if indentLevel > lastIndent:
-
-        change = indentLevel-lastIndent
-
-        isList = True
-        try:
-            if levelData.isTopList():
-
-                listIndexLevel = levelData.getTopIndentLevel()
-
-                if change <= 1:
-                    indentLevel = listIndexLevel
-                else:
-                    isList = False
-            else:
-                isList = False
-                
-        except:
-            isList = False
-        
-        if not isList:
-            levelData.addToPath(previousKey)
-            indentKeys[lastIndent] = previousKey
-
-    elif indentLevel < lastIndent:
-        levelData.removeFromPath(lastIndent - indentLevel)
-
-    lastIndent = indentLevel
+    #TODO Add stuff
 
     if firstCharacter == "-":
-        if not levelData.isTopList():
-            
-            print(indentLevel)
-            levelData.convertTopToList(indentLevel)
+        pass
+        #Is list item
+    else:
+        colonLocation = line.find(":")
 
-        afterDash = line[line.find("-")+1:]
+        keyText = line[leadingSpaces:colonLocation]
+        valueText = line[colonLocation+1:len(line)].lstrip(" ")
         
-        levelData.navigatePath()["data"] = afterDash.lstrip(" ")
+        print(f"{keyText} : {repr(valueText)}")
 
-        levelData.incrementIndexOnPath(levelData.getTopOfPath())
 
-    elif firstCharacter.isalpha():
 
-        #Assignment stuff
-        keyText = line[leadingSpaces:line.find(":")]
-        
-        valueText = line[line.find(":")+1:len(line)].lstrip(" ")
-
-        if valueText == "\n":
-            previousKey = keyText
-            
-            valueText = {}
-
-        #print(levelData.currentPath)
-        #print(levelData.navigatePath())
-        levelData.navigatePath()[keyText] = valueText
-
-print(levelData.levelData)
-#     currentIndentLevel = 0
-
-#     for i, char in enumerate(line):
-#         if char == " ":
-#             currentIndentLevel += 1
-    
-#     currentIndentLevel //= 2
+    #print(f"{indentLevel} :: {firstCharacter}")
         
