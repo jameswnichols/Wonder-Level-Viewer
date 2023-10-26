64eb4acf1d1ae83054aa5a891b8f5bc6906f1605
James Nichols <jnichols.tigley@gmail.com>
2023-10-26 19:13:19 +0100

    Actually somewhat works now.

--- toolkitYamlToJson.py
+++ toolkitYamlToJson.py
@@ -7,32 +7,51 @@ class LevelData:
         self.levelData = {}
         self.currentPath = {}
 
-    def navigatePath(self):
-        counter = 0
+    def getCurrentStructure(self, ignoreFinalIndex : bool = False):
         currentLevel = self.levelData
+        pathIndex = 0
         for level, val in self.currentPath.items():
+
             isList, index = val["isList"], val["index"]
 
-            if (not isList):
+            if (not isList) or (ignoreFinalIndex and pathIndex == len(self.currentPath)-1):
                 currentLevel = currentLevel[level]
             else:
                 currentLevel = currentLevel[level][index]
 
-            counter += 1
+            pathIndex += 1
 
         return currentLevel
+
+    def addStructure(self, key, isList):
+
+        value = [] if isList else {}
+        self.getCurrentStructure()[key] = value
+
+        self.currentPath[key] = {"isList":isList,"index":-1}
+
+    def removeTopStructure(self):
+        del self.currentPath[list(self.currentPath.keys())[-1]]
+
+    def increaseIndexOfTopList(self):
+        self.currentPath[list(self.currentPath.keys())[-1]]["index"] += 1
+        self.getCurrentStructure(ignoreFinalIndex=True).append({})
+
+def getIndentAndStartCharacter(line):
+    leadingSpaces = len(line) - len(line.lstrip(' '))
+
+    indentLevel = leadingSpaces // 2
+
+    firstCharacter = line[leadingSpaces]
+
+    return leadingSpaces, indentLevel, firstCharacter
+    
 
 yamlData = None
 
 levelData = LevelData()
 
-indentKeys = {}
-
-lastIndent = 0
-
-previousKey = None
-
-currentListIndex = 0
+lastIndentLevel = 0
 
 with open("TESTING.yaml","r") as f:
     yamlData = f.readlines()
@@ -40,26 +59,45 @@ with open("TESTING.yaml","r") as f:
 for i in range(0, 41):
     line = yamlData[i]
 
-    leadingSpaces = len(line) - len(line.lstrip(' '))
-
-    indentLevel = leadingSpaces // 2
-
-    firstCharacter = line[leadingSpaces]
+    leadingSpaces, indentLevel, firstCharacter = getIndentAndStartCharacter(line)
 
     #TODO Add stuff
 
     if firstCharacter == "-":
-        pass
+        indentLevel += 1
+
+        if indentLevel < lastIndentLevel:
+            levelData.removeTopStructure()
+
+        levelData.increaseIndexOfTopList()
+
+        itemData = line[line.find("- ")+1:]
+        print(itemData)
+
         #Is list item
     else:
         colonLocation = line.find(":")
 
         keyText = line[leadingSpaces:colonLocation]
-        valueText = line[colonLocation+1:len(line)].lstrip(" ")
-        
-        print(f"{keyText} : {repr(valueText)}")
+        valueText = line[colonLocation+1:len(line)]
 
+        isList = getIndentAndStartCharacter(yamlData[i + 1])[2] == "-"
+        isDict = valueText == "\n" and not isList
 
+        if indentLevel < lastIndentLevel:
+            levelData.removeTopStructure()
 
-    #print(f"{indentLevel} :: {firstCharacter}")
-        
+        # print(f"{keyText} : {repr(valueText)} :: IsList : {isList} IsDict : {isDict}")
+
+        if not isList and not isDict:
+            levelData.getCurrentStructure()[keyText] = valueText
+
+        else:
+            print(f"{keyText} :: {indentLevel}")
+            levelData.addStructure(keyText, isList)
+
+    lastIndentLevel = indentLevel
+
+    #
+
+print(levelData.levelData)
