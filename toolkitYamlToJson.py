1e79e8cfedf0ddcca3e7632800dc5130c22ef816
James Nichols <jnichols.tigley@gmail.com>
2023-10-26 12:14:13 +0100

    Started something?!?!

--- toolkitYamlToJson.py
+++ toolkitYamlToJson.py
@@ -1,1 +1,27 @@
 import json
+
+#5 places of float precision.
+
+yamlData = None
+
+levelData = {}
+
+lastIndent = 0
+
+currentPath = []
+
+with open("TESTING.yaml","r") as f:
+    yamlData = f.readlines()
+
+for line in yamlData:
+
+    currentIndentLevel = 0
+
+    for i, char in enumerate(line):
+        if char == " ":
+            currentIndentLevel += 1
+    
+    currentIndentLevel //= 2
+
+    if currentIndentLevel >
+        
